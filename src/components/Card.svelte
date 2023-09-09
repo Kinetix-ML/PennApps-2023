b08ae98ad29d304f6d8215eb911755abe1b23f69
Gus Ruben <gusruben@protonmail.com>
2023-09-09 16:21:02 -0400

    Add cards

---
+++ src/components/Card.svelte
@@ -0,0 +1,39 @@
+<script lang="ts">
+    export let width = "w-80";
+    export let height = "h-80";
+    export let opacity = "opacity-50";
+    export let index: number;
+    export let currentIndex: number;
+
+    let heightClass: string;
+    let opacityClass: string;
+    let colorClass: string;
+    function updateSize() {
+        if (currentIndex == index) {
+            heightClass = "h-[600px]";
+            opacityClass = "opacity-80";
+            colorClass = "bg-white-50";
+        } else if (Math.abs(currentIndex - index) == 1) {
+            heightClass = "h-96";
+            opacityClass = "opacity-60";
+            colorClass = "bg-white-100";
+        } else {
+            heightClass = height;
+            opacityClass = opacity;
+            colorClass = "bg-white-200";
+        }
+    }
+    updateSize();
+    
+    $: {
+        // this only works with the console.log for some reason
+        console.log(currentIndex);
+        updateSize();
+    }
+
+</script>
+
+<div class={`card ${width} ${heightClass} ${opacityClass} ${colorClass}
+bg-[rgba(235,235,235)] shadow-glass rounded-xl backdrop-blur-xl transition-all`}>
+    <slot />
+</div>
